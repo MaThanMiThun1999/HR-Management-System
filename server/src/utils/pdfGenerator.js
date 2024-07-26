13d6bb7506589bcb63e793f29c5f8e6d2ce7666f
MaThanMiThun <110737618+MaThanMiThun1999@users.noreply.github.com>
2024-07-27 03:09:21 +0530

    Update pdfGenerator.js

--- server/src/utils/pdfGenerator.js
+++ server/src/utils/pdfGenerator.js
@@ -16,7 +16,7 @@ const generateSalarySlip = async (employee, salary, attendance) => {
         const pdfDoc = new PDFDocument({ size: 'A4', margin: 50 });
 
         // Define the path where the PDF will be saved
-        const pdfPath = path.join(__dirname,'..','..','..','uploads', 'PDF', `${employee.name.replace(/\s/g, '-')}-salary-${new Date(salary.date).getMonth() + 1}-${new Date(salary.date).getFullYear()}.pdf`);
+        const pdfPath = path.join(__dirname,'..','..','..','..','..','..','uploads', 'PDF', `${employee.name.replace(/\s/g, '-')}-salary-${new Date(salary.date).getMonth() + 1}-${new Date(salary.date).getFullYear()}.pdf`);
 
         // Create a write stream to the specified path
         pdfDoc.pipe(fs.createWriteStream(pdfPath));
