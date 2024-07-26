f10b2bae3453dbf4315f47214779da43dcd5a7c4
MaThanMiThun <110737618+MaThanMiThun1999@users.noreply.github.com>
2024-07-26 23:59:23 +0530

    Update axiosInstance.js

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://hr-management-backend-eczm.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
